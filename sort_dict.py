f652fe55c454dd4bc02bbaa89d7483132038ef14
Tirtha <tirthasg@gmail.com>
2025-03-31 14:22:18 +0530

    feat: sort_dict.py added to repo

---
+++ sort_dict.py
@@ -0,0 +1,11 @@
+def sort_dict(input_dict):
+    return dict(sorted(input_dict.items()))
+
+
+original_dict = {1: "N", 6: "!", 4: "T", 3: "A", 2: "E", 5: "O"}
+print(sort_dict(original_dict))
+# {1: 'N', 2: 'E', 3: 'A', 4: 'T', 5: 'O', 6: '!'}
+
+color_counts = {"red": 3, "blue": 4, "green": 5, "yellow": 7}
+print(sort_dict(color_counts))
+# {'blue': 4, 'green': 5, 'red': 3, 'yellow': 7}
