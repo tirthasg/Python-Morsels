8bc293f3d8662e2f9e87046e9eb2770566a554e1
Tirtha <tirthasg@gmail.com>
2025-04-02 21:36:05 +0530

    chore: missing type annotations added

--- is_anagram.py
+++ is_anagram.py
@@ -1,12 +1,12 @@
 from string import whitespace, punctuation
 
 
-def clean_string(string):
+def clean_string(string: str) -> str:
     black_list = set(whitespace + punctuation)
     return "".join([char.lower() for char in string if char not in black_list])
 
 
-def is_anagram(s1, s2):
+def is_anagram(s1: str, s2: str) -> bool:
     s1 = clean_string(s1)
     s2 = clean_string(s2)
 
