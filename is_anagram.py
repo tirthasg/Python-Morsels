1e1214e226e44e0f7512ae3dac8e7816349ea81b
Tirtha <tirthasg@gmail.com>
2025-04-02 21:32:08 +0530

    feat: is_anagram.py added to repo

---
+++ is_anagram.py
@@ -0,0 +1,17 @@
+from string import whitespace, punctuation
+
+
+def clean_string(string):
+    black_list = set(whitespace + punctuation)
+    return "".join([char.lower() for char in string if char not in black_list])
+
+
+def is_anagram(s1, s2):
+    s1 = clean_string(s1)
+    s2 = clean_string(s2)
+
+    return sorted(s1) == sorted(s2)
+
+
+print(is_anagram("coins kept", "in pockets"))
+print(is_anagram("a diet", "I'd eat"))
