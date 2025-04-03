972aaccac77cb71e4356ca0e4c09e92628b57fa8
Tirtha <tirthasg@gmail.com>
2025-04-03 22:41:43 +0530

    refactor: testing equality via tuple comparison

--- point.py
+++ point.py
@@ -8,7 +8,7 @@ class Point:
         return f"Point(x={self.x}, y={self.y}, z={self.z})"
 
     def __eq__(self, other: "Point"):
-        return self.x == other.x and self.y == other.y and self.z == other.z
+        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
 
     def __add__(self, other: "Point"):
         return Point(self.x + other.x, self.y + other.y, self.z + other.z)
