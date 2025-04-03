358ac26cb33ad9a93e72b9f962fb7137195f2dee
Tirtha <tirthasg@gmail.com>
2025-04-03 22:23:29 +0530

    feat: few more ops supported for point class

--- point.py
+++ point.py
@@ -10,6 +10,21 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.z == other.z
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, factor):
+        return Point(self.x * factor, self.y * factor, self.z * factor)
+
+    def __rmul__(self, factor):
+        return self.__mul__(factor)
+
+    def __iter__(self):
+        return iter((self.x, self.y, self.z))
+
 
 p1 = Point(1, 2, 3)
 print(p1)
@@ -18,3 +33,16 @@ p2 = Point(1, 2, 3)
 print(p1 == p2)
 p2.x = 4
 print(p1 == p2)
+
+p1 = Point(1, 2, 3)
+p2 = Point(4, 5, 6)
+print(p1 + p2)
+print(p2 - p1)
+
+p1 = Point(1, 2, 3)
+print(p1 * 2)
+print(2 * p1)
+
+p1 = Point(1, 2, 3)
+x, y, z = p1
+print(x, y, z)
