56fb3bfdd18bb3046eb4f8f0ab8383c2cd7a0ff8
Tirtha <tirthasg@gmail.com>
2025-04-06 18:08:19 +0530

    chore: missing type annotations added

--- dictionaries/flip_dict.py
+++ dictionaries/flip_dict.py
@@ -1,21 +1,28 @@
-def flip_dict(record, *, error_on_duplicates=False):
-    if error_on_duplicates and len(record.values()) != len(set(record.values())):
+# def flip_dict(
+#     record: dict[str, str], *, error_on_duplicates: bool = False
+# ) -> dict[str, str]:
+#     if error_on_duplicates and len(set(record.values())) != len(record.values()):
+#         raise ValueError("Dictionary has duplicate values")
+
+#     return dict(zip(record.values(), record.keys()))
+
+
+def flip_dict(
+    record: dict[str, str], *, error_on_duplicates: bool = False
+) -> dict[str, str]:
+    if error_on_duplicates and len(set(record.values())) != len(record.values()):
         raise ValueError("Dictionary has duplicate values")
 
-    return {old_value: old_key for old_key, old_value in record.items()}
-    # return {
-    #     old_value: old_key
-    #     for old_key, old_value in record.items()
-    # }
-
-
-# def flip_dict(old_dict):
-#     return dict(zip(old_dict.values(), old_dict.keys()))
+    return {value: key for key, value in record.items()}
 
 
 print(flip_dict({"Python": "2015-09-15", "Java": "2015-09-14", "C": "2015-09-13"}))
+# {'2015-09-15': 'Python', '2015-09-14': 'Java', '2015-09-13': 'C'}
 print(flip_dict({"Python": "2015-09-15", "Java": "2015-09-14", "C": "2015-09-15"}))
+# {'2015-09-15': 'C', '2015-09-14': 'Java'}
 
 pairs = {"Garnet": "Amethyst", "Steven": "Rose", "Pearl": "Rose"}
 print(flip_dict(pairs))
+# {"Amethyst": "Garnet", "Rose": "Pearl"}
 print(flip_dict(pairs, error_on_duplicates=True))
+#  ValueError Exception
