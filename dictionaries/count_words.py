a5db8b75d257253bb42c05571c47c866b6d275f6
Tirtha <tirthasg@gmail.com>
2025-04-06 16:48:05 +0530

    chore: expected outputs added as comment

--- dictionaries/count_words.py
+++ dictionaries/count_words.py
@@ -12,8 +12,10 @@ def count_words(sentence: str) -> dict[str, int]:
 
 
 print(count_words("oh what a day what a lovely day"))
+# {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
 print(count_words("don't stop believing"))
-
+# {"don't": 1, 'stop': 1, 'believing': 1}
 print(count_words("The Queen will address the House of Commons today"))
-
+# {"the": 2, "queen": 1, "will": 1, "address": 1, "house": 1, "of": 1, "commons": 1, "today": 1,}
 print(count_words("Oh what a day, what a lovely day!"))
+# {"oh": 1, "what": 2, "a": 2, "day": 2, "lovely": 1}
